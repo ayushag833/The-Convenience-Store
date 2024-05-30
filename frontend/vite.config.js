71401ff78e6084540928fcd4f64d490adefea2e9
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-05-30 17:45:36 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://the-convenience-store.onrender.com/",
+      "/uploads/": "https://the-convenience-store.onrender.com/",
     },
   },
 });
