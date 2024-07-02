17586291ffbe13d708a927c96f080302a7dc8f70
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-07-02 15:44:15 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-res.cookie("abc", "abcdef");
-
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
