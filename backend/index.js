e9b159b73db0bbd1d99d1f348df572e2dcfc42d2
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-07-02 17:52:55 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://the-convenience-store.netlify.app/"],
+    origin: "https://the-convenience-store.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
