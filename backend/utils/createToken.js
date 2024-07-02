97b565725d16a9af1dcf13104ea59bb7843e1390
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-07-02 14:31:23 +0530

    commit

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,12 +6,14 @@ const generateToken = (res, userId) => {
   });
 
   // Set JWT as an HTTP Only Cookie
-  res.cookie("JWT", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-  });
+  // res.cookie("JWT", token, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV !== "development",
+  //   sameSite: "strict",
+  //   maxAge: 30 * 24 * 60 * 60 * 1000,
+  // });
+  res.cookie("JWT", token);
+  console.log(token);
   return token;
 };
 
